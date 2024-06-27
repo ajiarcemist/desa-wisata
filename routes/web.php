be37dc8882c87e9349f684eedc76c3b6b52cd430
ajiarcemist <ajiarcemist@gmail.com>
2024-06-27 16:28:43 +0000

    first commit

---
+++ routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AuthController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', [HomeController::class, 'index']);
+
+Route::get('/login', [AuthController::class, 'login']);
+
+Route::post('/login', [AuthController::class,'authenticated']);
